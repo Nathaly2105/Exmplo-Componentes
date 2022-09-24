dc7afe0c9dd3d1edddc3c07f33d1dd9ffcfe6b83
Nathaly2105 <nathaly.rocha.est@gmail.com>
2022-09-24 01:00:27 +0000

    Componente

--- App.js
+++ App.js
@@ -1,11 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import Dados from './components/Dados';
 
 export default function App() {
+	const [nome, setNome] = useState("");
+	const [nomeParametro, setNomeParametro]=useState("");
+	const [idade, setIdade]=useState("");
+	const [idadeParametro, setIdadeParametro]=useState("");
+	
+	function gravar(){
+		setNomeParametro(nome);
+		setIdadeParametro(idade);
+	}
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+		  
+		  <View style={styles.bloco}>
+			  <Text style={styles.label}>Nome:</Text>
+			  <TextInput 
+				  style={styles.input}
+				  value={nome}
+				  onChangeText={(value)=>setNome(value)}
+			/>
+		  </View>
+		  
+		  <View style={styles.bloco}>
+			  <Text style={styles.label}>Idade:</Text>
+			  <TextInput
+				  style={styles.input}
+				  value={idade}
+				  onChangeText={(value)=>setIdade(value)}
+			   />
+		  </View>
+		  
+		  <View style={styles.bloco}>
+			  <TouchableOpacity style={styles.botao} onPress={gravar}>
+				  <Text style={styles.textoBotao}>Mostrar</Text>
+			  </TouchableOpacity>
+		  </View>
+		  
+		  <View style={styles.bloco}>
+			  <Dados nome={nomeParametro} idade={idadeParametro}/>
+		  </View>
     </View>
   );
 }
@@ -13,8 +49,28 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  bloco:{
+	 width:'80%',
+	 marginTop:30,
+	 marginLeft:'10%'
+  },
+  label:{
+	  fontSize:20,
+  },
+  input:{
+	 borderWidth:1,
+	 fontSize:20,
+	 borderRadius: 5
+  },
+  botao:{
+	 backgroundColor:'#FF69B4',
+	 borderRadius:10,
+	 width: 200
+  },
+  textoBotao:{
+	  color:'#fff',
+	  fontSize:20,
+	  textAlign:'center'
+  }
 });
